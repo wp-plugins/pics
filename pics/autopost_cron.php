220226737a38a19795d63081cefb389e65f075ea
TIAFO <TIAFO@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-06-22 03:38:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/pics/trunk@51400 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pics/autopost_cron.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: PICS
+Plugin URI: http://www.tiafo.com
+Description: This is a plugin of Picture Increment Cron System (PICS).
+Author:  the TIAFO team
+Version: 1.0
+Author URI: http://www.tiafo.com
+*/ 
+
+
+require_once( 'include/lib.php' );
+$newpics = new PICSMain();
+?>
